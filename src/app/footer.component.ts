8e35e5861a261b351f699673af12fe07153a37e5
P.T Nyamuda <ptnrlab@gmail.com>
2023-08-10 11:19:41 +0200

    footer links added

--- src/app/footer.component.ts
+++ src/app/footer.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, Signal } from '@angular/core';
+import { CoursesService } from './courses/courses.service';
+import { Course } from './courses/course.model';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  constructor(private courseService: CoursesService) {}
 
+  ngOnInit(): void {}
+
+  //Get the the first 3 courses from
+  //a list of all courses
+  courses: Signal<Course[]> = computed(() =>
+    this.courseService
+      .courseListSignal()
+      .filter((val: Course, index: number) => index < 3)
+  );
 }
