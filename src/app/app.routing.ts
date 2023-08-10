9b932c9ffd4e49cf489eae7331c13f6cc0475f8a
P.T Nyamuda <ptnrlab@gmail.com>
2023-08-10 12:51:38 +0200

    header fixed

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -15,9 +15,9 @@ import { authGuard, adminAuthGuard } from './auth/auth.guard';
 import { HomepageComponent } from './homepage/homepage.component';
 
 export const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
-  { path: '', component: HomepageComponent },
+  { path: 'home', component: HomepageComponent },
 
   {
     path: 'courses',
