25cf054266d2cd8390ce9633b687efbf30ee14ff
P.T Nyamuda <ptnrlab@gmail.com>
2023-08-10 15:00:51 +0200

    course card space reduced

--- src/app/homepage/homepage-courses/homepage-courses.component.ts
+++ src/app/homepage/homepage-courses/homepage-courses.component.ts
@@ -1,4 +1,4 @@
-import { Component, Signal, computed } from '@angular/core';
+import { Component, Signal, computed, OnInit } from '@angular/core';
 import { Course } from 'src/app/courses/course.model';
 import { CoursesService } from 'src/app/courses/courses.service';
 
@@ -7,10 +7,15 @@ import { CoursesService } from 'src/app/courses/courses.service';
   templateUrl: './homepage-courses.component.html',
   styleUrls: ['./homepage-courses.component.scss'],
 })
-export class HomepageCoursesComponent {
+export class HomepageCoursesComponent implements OnInit {
   placeholderCards: number[] = [0, 1, 2];
   constructor(private courseService: CoursesService) {}
 
+  ngOnInit(): void {
+    if (this.courses.length == 0) {
+      this.courseService.getCourses();
+    }
+  }
   //Get the the first 3 courses from
   //a list of all courses
   courses: Signal<Course[]> = computed(() =>
