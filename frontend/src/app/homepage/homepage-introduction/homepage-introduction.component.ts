0b1f4cb282ae55944e2abfc56a8e1b753a0a30f9
P.T Nyamuda <ptnrlab@gmail.com>
2023-11-14 12:41:17 +0200

    illustrations added

--- frontend/src/app/homepage/homepage-introduction/homepage-introduction.component.ts
+++ frontend/src/app/homepage/homepage-introduction/homepage-introduction.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage-introduction',
   templateUrl: './homepage-introduction.component.html',
-  styleUrls: ['./homepage-introduction.component.scss']
+  styleUrls: ['./homepage-introduction.component.scss'],
 })
-export class HomepageIntroductionComponent {
+export class HomepageIntroductionComponent implements OnInit {
+  imageNumber: number = 1;
 
+  ngOnInit(): void {
+    this.imageNumber = this.generateRandomNumber();
+  }
+
+  //Generate random number between 1 and 3
+  generateRandomNumber(): number {
+    // Generate a random number between 1 and 3
+    return Math.floor(Math.random() * 4) + 1;
+  }
 }
