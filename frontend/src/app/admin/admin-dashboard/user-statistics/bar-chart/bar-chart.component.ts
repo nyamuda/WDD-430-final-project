eedff5f9f490bb7685283c24975ef628fd862dd8
P.T Nyamuda <ptnrlab@gmail.com>
2023-11-23 16:14:59 +0200

    change  detection added to bar chart component

--- frontend/src/app/admin/admin-dashboard/user-statistics/bar-chart/bar-chart.component.ts
+++ frontend/src/app/admin/admin-dashboard/user-statistics/bar-chart/bar-chart.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { Chart } from 'chart.js/auto';
 
 @Component({
@@ -6,17 +16,24 @@ import { Chart } from 'chart.js/auto';
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.scss'],
 })
-export class BarChartComponent implements OnInit {
+export class BarChartComponent implements AfterViewInit {
   chart: any = [];
+  @ViewChild('canvas') chartCanvas: ElementRef;
   @Input() data_1: bookingStatistic = { label: '', value: 0 };
   @Input() data_2: bookingStatistic = { label: '', value: 0 };
 
-  ngOnInit(): void {
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
     this.createChart();
+    this.cdr.detectChanges(); // Trigger change detection
   }
 
   createChart() {
-    this.chart = new Chart('canvas', {
+    const canvas: any = this.chartCanvas.nativeElement;
+    const ctx = canvas.getContext('2d');
+
+    this.chart = new Chart(ctx, {
       type: 'bar',
       data: {
         labels: [this.data_1.label, this.data_2.label],
